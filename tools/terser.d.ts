982efcb3fd602455771cbbb61d317b8dcdeb76dd
Holger Benl <hbenl@evandor.de>
2025-05-13 17:11:10 +0200

    add scopes to SourceMapOptions type

--- tools/terser.d.ts
+++ tools/terser.d.ts
@@ -210,6 +210,7 @@ export interface SourceMapOptions {
     root?: string;
     asObject?: boolean;
     url?: string | 'inline';
+    scopes?: boolean;
 }
 
 export function minify(files: string | string[] | { [file: string]: string }, options?: MinifyOptions): Promise<MinifyOutput>;
